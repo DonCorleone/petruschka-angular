97eccdbe4c7ff03b7cc30c37161de1a12f66cfb8
DonCorleone <vitocorleone77@gmail.com>
2020-10-15 23:59:56 +0200

    load eventgroup on card

--- src/app/event-group-card/event-group-card.component.ts
+++ src/app/event-group-card/event-group-card.component.ts
@@ -8,15 +8,15 @@ import { EventGroup, EventGroupsService } from '../event-groups.service';
   styleUrls: ['./event-group-card.component.scss']
 })
 export class EventGroupCardComponent implements OnInit {
+  eventgroup: any;
 
   constructor(private eventGroupsService: EventGroupsService) { }
 
   @Input() eventgroupId: number;
-  @Input() eventgroup: EventGroup;
   @Input() type: 'horizontal' | 'vertical' = 'vertical';
   @Input() showBuyButton: boolean;
 
   ngOnInit(): void {
-    this.eventGroupsService.getEventGroupById(this.eventgroupId).subscribe(p => this.eventgroup = p);
+    this.eventgroup = this.eventGroupsService.getEventGroupById(this.eventgroupId).subscribe(p => this.eventgroup = p);
   }
 }
