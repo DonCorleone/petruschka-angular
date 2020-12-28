a4d982911cce3e09ae5ac405f58575c38ae3e029
Linus <vitocorleone77@gmail.com>
2020-12-28 20:44:32 +0100

    graphql as module removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,24 @@ import { EventGroupsComponent } from './event-groups/event-groups.component';
 import { EventGroupCardComponent } from './event-group-card/event-group-card.component';
 import { EventgroupDetailsComponent } from './eventgroup-details/eventgroup-details.component';
 import { EventCardComponent } from './event-card/event-card.component';
-import { GraphQLModule } from './graphql.module';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import {APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import {HttpLink} from 'apollo-angular/http';
+
+import * as realm from './realm';
+import { HttpHeaders } from '@angular/common/http';
+const uri = realm.graphqlUrl;
+
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+	return {
+		link: httpLink.create({
+			uri,
+			headers: new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`)
+		}),
+		cache: new InMemoryCache()
+	};
+}
 
 @NgModule({
   declarations: [
@@ -26,10 +42,15 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
     AppRoutingModule,
     // ScullyLibModule,
     HttpClientModule,
-    GraphQLModule,
     ScullyLibModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
