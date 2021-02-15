13181903c897316a51c21f868948daad89dbdbbd
Don Corleone <vitocorleone77@gmail.com>
2021-02-15 12:42:37 +0100

    Revert "Bootstrap"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import { HttpHeaders } from '@angular/common/http';
 import { LocationComponent } from './components/location/location.component';
 import { StaffComponent } from './components/staff/staff.component';
 import { StaffCardComponent } from './components/staff-card/staff-card.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 const uri = realm.graphqlUrl;
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
@@ -49,7 +48,6 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
     AppRoutingModule,
     HttpClientModule,
     ScullyLibModule,
-    NgbModule,
   ],
   providers: [
     {
